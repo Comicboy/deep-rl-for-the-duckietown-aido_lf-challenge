2a0090ba0adef9d73c1e019a8b0ca38f85ad43e3
Balazs <peiszbalazs@gmai.com>
2020-10-25 20:04:36 +0100

    First version of the setup file, incomplete

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This shell script helps to setup the needed environment for recreating our work with the duckietown RL algorithm
+
+# First of all we install python3 and also pip3, git and git-ls
+sudo apt-get install python3
+sudo apt-get install -y python3-pip git git-lfs
+
+# Next we need to install docker using the repository
+# Uninstalling older versions of docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Setting up the repository
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Installing docker engine
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#Verify docker's installation
+sudo docker run hello-world
+
+# Next up we need to install duckietown shell, which enables us to use the duckietown devolopment environment through useful shell scripts
+pip3 install --no-cache-dir --user -U duckietown-shell
+
+#Verifying the installation
+which dts
+dts-version
+
+
